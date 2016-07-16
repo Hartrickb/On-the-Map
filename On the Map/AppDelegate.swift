58f87b6d9479965f8a37a3f34a708d9cfc378cd3
“Hartrickb” <Hartrickb@users.noreply.github.com>
2016-07-16 11:56:18 -0400

    Organized the Login Process
    - Created a UdacityClient
    - Created a taskforPOST method
    - Created a postSession method for logging in to udacity
    - Implemented errors that are displayed to the user in a friendly
    syntax if something goes wrong
    - Moved UserId storage to the Model (UdacityClient)
    - Created a Constants file for maintainability

--- On the Map/AppDelegate.swift
+++ On the Map/AppDelegate.swift
@@ -14,10 +14,6 @@ import MapKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    var student = StudentInformation()
-    
-    var userID: String?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
---
+++ On the Map/UdacityClient.swift
@@ -0,0 +1,107 @@
+//
+//  UdacityClient.swift
+//  On the Map
+//
+//  Created by Bennett Hartrick on 7/15/16.
+//  Copyright © 2016 Bennett. All rights reserved.
+//
+
+import Foundation
+
+class UdacityClient: NSObject {
+    
+    // Shared Session
+    var session = NSURLSession.sharedSession()
+    
+    // Info Storage
+    var userID: String?
+    var student = StudentInformation()
+    
+    override init() {
+        super.init()
+    }
+    
+    // MARK: POST
+    
+    func taskForPOSTMethod(jsonBody jsonBody: String, completionHandlerForPost: (results: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
+        
+        /* 1. Set the parameters */
+        // There are none...
+        
+        /* 2/3. Build the URL, Configure the request */
+        let request = NSMutableURLRequest(URL: NSURL(string: UdacityClient.baseURL)!)
+        request.HTTPMethod = "POST"
+        request.addValue(Constants.appJSON, forHTTPHeaderField: HTTPHeaders.accept)
+        request.addValue(Constants.appJSON, forHTTPHeaderField: HTTPHeaders.contentType)
+        request.HTTPBody = jsonBody.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        print("Request: \(request)")
+        print("Body: \(request.HTTPBody)")
+        print(jsonBody)
+        /* 4. Make the request */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            func sendError(error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey: error]
+                completionHandlerForPost(results: nil, error: NSError(domain: "taskForPOSTMethod", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard error == nil else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2xx response? */
+//            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+//                sendError("Your request returned a status code other than 2xx!")
+//                return
+//            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            self.convertDataWithCompletionHandler(data, completionHandlerForConvertData: completionHandlerForPost)
+            
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+        
+        return task
+    }
+    
+    // Given raw JSON, return a usable Foundation object
+    private func convertDataWithCompletionHandler(data: NSData, completionHandlerForConvertData: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject!
+        do {
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+            parsedResult = try NSJSONSerialization.JSONObjectWithData(newData, options: .AllowFragments)
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey: "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(result: nil, error: NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerForConvertData(result: parsedResult, error: nil)
+    }
+    
+    class func sharedInstance() -> UdacityClient {
+        struct Singleton {
+            static var sharedInstance = UdacityClient()
+        }
+        return Singleton.sharedInstance
+    }
+}
+
+
+
+
+
+
+
